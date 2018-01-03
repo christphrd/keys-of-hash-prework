11609e1c033e65b4422b051e9fb6be02280ca4c9
Christopher Diep <diep.christopher@gmail.com>
2018-01-03 21:31:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,13 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     values_array = [*arguments]
+    binding.pry
     values_array.map do |e|
+      binding.pry
       self.each do |k,v|
+        binding.pry
         if v == e
+          binding.pry
           k
         end
       end
